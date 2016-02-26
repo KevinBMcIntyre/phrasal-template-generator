874fe76b0cc60380b4b0875cd73ba1ac6d383dc6
Kevin McIntyre <kevin@Kevins-MacBook-Pro.local>
2016-02-26 10:36:19 -0500

    worked on code

---
+++ js/main.js
@@ -0,0 +1,12 @@
+alert('Let’s make a story together!');
+var colour = prompt('A colour (orange, indigo, etc.)');
+var creature = prompt('A creature (dragon, moth, etc.)');
+var adjective = prompt('An adjective (beautiful, super, etc.)');
+var verbed = prompt('A past tense verb (ran, burped, etc.)');
+var story = prompt('Which story would you like?'/n'a) Snacking'/n'b) Escaping'/n'c) Winning'/n'Enter the letter below:');
+if (story == 'a')
+	write.document('After snacking on ' + adjective + ' treats, the ' + colour + ' bellied ' + creature + verbed + ' for hours.');
+if (story == 'b')
+	write.document('Ameilia ' + verbed + ' through the ' + adjective + colour + ' nebula escaping the space ' + creature + '.');
+if (story == 'c')
+	write.document('Jackson chose his ' + adjective + ' , ' + colour + creature + ' card and ' + verbed + ' it to the table knowing he won.');
